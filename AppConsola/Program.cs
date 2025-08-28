ed4ddfe6c540d48737732073738a8d334eb83cc2
KepperCommander <isaramirez5279@gmail.com>
2025-08-28 09:01:48 -0500

    4 commit

--- AppConsola/Program.cs
+++ AppConsola/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class Vehiculos
+{
+    public int Ruedas;
+    public string? Placa;
+    public bool Electrico;
+    public DateTime FechaDeMatricula;
+    public Tipos? Tipo;
+    public List<Revisiones>? Revisiones;
+}
+
